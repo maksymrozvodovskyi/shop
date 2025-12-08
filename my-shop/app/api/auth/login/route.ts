52fb7ca8687d6c72a66fe3b428c734a088f645a2
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-12-08 09:34:45 +0200

    test

--- my-shop/app/api/auth/login/route.ts
+++ my-shop/app/api/auth/login/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/serve";
 import { api } from "@/lib/api/api";
 import { cookies } from "next/headers";
 import type { StrapiErrorResponseType } from "@/lib/types/strapi";
